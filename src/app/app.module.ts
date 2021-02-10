7317f42e080959397c5c4d0eb25f6eed021a889e
Guillermo Mainini <guimainini@gmail.com>
2021-02-10 18:46:17 -0300

    hice los pipes numericos y los de texto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 //Modulo personalizado
 
-import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+// Cambiar el locale de la app
+import localeEs from "@angular/common/locales/es-AR";
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData( localeEs );
 
 
 
@@ -15,12 +22,18 @@ import { AppRouterModule } from './app-router.module';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRouterModule,
-    SharedModule
+    BrowserModule,
+    SharedModule,
+
+    VentasModule
     
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es-AR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent  {
 
-  constructor() { }
+  nombreLower    : string = 'guillermo';
+  nombreUpper    : string = 'GUILLERMO';
+  nombreCompletp : string = 'gUiLlErMo mAiNiNi';
 
-  ngOnInit(): void {
-  }
+  fecha: Date = new Date(); // el dia de hoy
+
+
+  
 
 }
